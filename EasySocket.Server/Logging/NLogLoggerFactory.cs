bceb3020c2c5d58af6ea9fa4ff53dca4277d4429
JeongTaeLee <jeongtae8633@gmail.com>
2021-05-01 04:18:32 +0900

    EasySocket.Server NLog 클래스 추가

---
+++ EasySocket.Server/Logging/NLogLoggerFactory.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using EasySocket.Common.Logging;
+
+namespace EasySocket.Server.Logging
+{
+    public class NLogLoggerFactory : LogFactoryBase
+    {
+        public NLogLoggerFactory(string config)
+            : base(config)
+        {
+            if (!IsSharedConfig)
+            {
+                NLog.Config.XmlLoggingConfiguration.SetCandidateConfigFilePaths(new[] { ConfigFile });
+            }
+            else
+            {
+            }
+        }
+
+        public override ILogger GetLogger(string name)
+        {
+            return new NLogLogger(NLog.LogManager.GetLogger(name));
+        }
+
+        public override ILogger GetLogger(Type type)
+        {            
+            return new NLogLogger(NLog.LogManager.GetLogger(nameof(type)));
+        }
+    }
+}
