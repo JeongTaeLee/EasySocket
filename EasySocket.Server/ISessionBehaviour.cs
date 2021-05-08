ae9447834a564e2c8d64c05f74a462bc1eedd71d
seongmin.mo <wisemin01@naver.com>
2021-05-08 21:53:00 +0900

    fixed - ISessionBehaviour 인터페이스 인자 이름 수정.

--- EasySocket.Server/ISessionBehaviour.cs
+++ EasySocket.Server/ISessionBehaviour.cs
@@ -5,10 +5,10 @@ namespace EasySocket.Server
 {
     public interface ISessionBehaviour
     {
-        ValueTask OnStartBeforeAsync(ISession ssn);
-        ValueTask OnStartAfterAsync(ISession ssn);
-        ValueTask OnStoppedAsync(ISession ValueTask);
-        ValueTask OnReceivedAsync(ISession ssn, object packet);
-        void OnError(ISession ssn, Exception ex);
+        ValueTask OnStartBeforeAsync(ISession session);
+        ValueTask OnStartAfterAsync(ISession session);
+        ValueTask OnStoppedAsync(ISession session);
+        ValueTask OnReceivedAsync(ISession session, object packet);
+        void OnError(ISession session, Exception ex);
     }
 }
